38afd06c249915c3763f08c8dc6550fa38db394b
Maxime FRANC <mfranc@e3r2p17.42.fr>
2017-06-09 19:58:55 +0200

    update

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/08 12:26:21 by mfranc            #+#    #+#             */
-/*   Updated: 2017/06/08 16:08:17 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/06/09 16:53:12 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,15 @@
 #include "libft.h"
 #include "ft_printf.h"
 #include "get_next_line.h"
+
+typedef struct	s_gamedata
+{
+	int			plate_dim[2];
+	int			last_pieceopp_coordstart[2];
+	int			last_pieceopp_coordend[2];
+	int			gap_between_pieces[2];
+	int			opp_score;
+	int			piece_dim[2];
+}				t_gamedata;
 
 #endif
--- srcs/filler.c
+++ srcs/filler.c
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/08 12:25:52 by mfranc            #+#    #+#             */
-/*   Updated: 2017/06/08 18:41:05 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/06/09 18:19:20 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,45 @@ int		ft_exit_filler(void)
 	exit(-1);
 }
 
-void	ft_state_buff(char *buff, int *pi)
+t_gamedata	ft_init_gamedata(void)
 {
-	int		(*infos[3](char *, int *);
-	char	index_state;
+	t_gamedata	gamedata;
 
+	gamedata.plate_dim[0] = 0;
+	gamedata.plate_dim[1] = 0;
+	gamedata.last_pieceopp_coordstart[0] = 0;
+	gamedata.last_pieceopp_coordstart[1] = 0;
+	gamedata.gap_between_pieces[0] = 0;
+	gamedata.gap_between_pieces[1] = 0;
+	gamedata.opp_score = 0;
+	gamedata.piece_dim[0] = 0;
+	gamedata.piece_dim[1] = 0;
+	return (gamedata);
 }
 
 int		main(void)
 {
-	int		ret;
-	int		i;
-	char	buff[BUFF_SIZE];
-
-	while ((ret = read(0, buff, BUFF_SIZE) != 0))
-		buff[ret] = '\0';
-	if (ret == -1)
-		return (ft_exit_filler());
+//	int			ret;
+	int			i;
+	char		buff[BUFF_SIZE];
+	t_gamedata	gamedata;
+	
+	gamedata = ft_init_gamedata();
+//	if ((ret = read(0, buff, BUFF_SIZE) == -1))
+//		ft_exit_filler();
+//	buff[ret] = '\0';
+	PSTR(buff)
+//	if (ret == -1)
+//		return (ft_exit_filler());
 	i = -1;
-	while (buff[++i])
-		ft_state_buff(buff + i, &i);
-	ft_printf("coucou\n");
+/*	while (buff[++i])
+	{
+		if (ft_strequ(buff + i, "Plateau"))
+			ft_init_game(buff + i, &i, &gamedata);
+		else if (ft_strequ(buff + i, "\n0"))
+			ft_update_point(buff + i, &i, &gamedata);
+		else if (ft_strequ(buff + i, "Piece"))
+			ft_get_piece(buff + i, &i, &gamedata);
+	}*/
 	return (0);
 }
